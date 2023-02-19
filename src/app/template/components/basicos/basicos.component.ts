b1b1ab8941e437f619b9b4ed7fa20d30cfc2f0ab
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-19 11:49:52 -0600

    mensajes de error

--- src/app/template/components/basicos/basicos.component.ts
+++ src/app/template/components/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  guardar(formulario: NgForm){
+    console.log("entro", formulario.value);
+    
+  }
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './components/basicos/basicos.component';
 import { DinamicosComponent } from './components/dinamicos/dinamicos.component';
 import { SwitchesComponent } from './components/switches/switches.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
+      CommonModule,
+      FormsModule,
       TemplateRoutingModule
     ],
     declarations: [

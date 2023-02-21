976cc1e8563b99892892decba7107050c52d97f8
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-20 23:55:52 -0600

    agregar formulario dinamico

--- src/app/template/components/dinamicos/dinamicos.component.ts
+++ src/app/template/components/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,4 +17,43 @@ import { Component } from '@angular/core';
 })
 export class DinamicosComponent {
 
+  @ViewChild('miFormulario') formulario!: NgForm;
+
+  nuevoJuego: string = "";
+
+  persona: Persona = {
+    nombre: 'Adrian',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre: 'Halo'
+      }
+    ]
+  }
+
+  nombreValido():boolean{
+    return this.formulario?.controls?.['nombre']?.invalid 
+            && this.formulario?.controls?.['nombre']?.touched
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index,1);
+  }
+  
+  agregarJuego(){
+    const juego: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+    this.persona.favoritos.push({...juego});
+    this.nuevoJuego = '';
+  }
+
+  guardar(){
+    
+  }
 }

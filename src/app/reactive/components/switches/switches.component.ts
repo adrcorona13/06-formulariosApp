00ba6961a1bbcd8114f151958f2f823556206f4e
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-23 18:43:31 -0600

    finalizar switches reactivos

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
 
+  // terminos: FormControl = this.formBuilder.control(['', Validators.required])
+
+  formulario: FormGroup = this.formBuilder.group({
+    genero: ['M', Validators.required],
+    notificaciones: ['', Validators.required],
+    terminos: ['false', Validators.requiredTrue]
+  })
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.formulario.reset(this.persona);
+    this.formulario.reset({
+      ...this.persona,
+      terminos: true
+    });
+
+    // this.formulario.get('terminos')?.valueChanges.subscribe(value => {
+    //   console.log(value);
+    // })
+
+    this.formulario.valueChanges.subscribe(({terminos, ...rest}) => {      
+      this.persona = rest;
+    })
+  }
+
+  guardar() {
+    const value = { ...this.formulario.value };
+    delete value['terminos'];
+    this.persona = value;
+
+    console.log(value);
+
+  }
 }

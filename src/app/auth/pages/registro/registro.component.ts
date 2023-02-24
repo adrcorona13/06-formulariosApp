d15305249f87087b62ed201281c2879eccad8a73
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-24 01:18:35 -0600

    agregar mensaje personalizado

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -9,7 +9,7 @@ import { EmailValidatorService } from '../../../shared/services/emailValidator.s
     selector: 'app-registro',
     templateUrl: 'registro.component.html'
 })
-export class RegistroComponent implements OnInit { 
+export class RegistroComponent implements OnInit {
 
     formulario: FormGroup = this.fb.group({
         nombre: ['', [Validators.required, Validators.pattern(this.validatorService.regexNombreApellido)]],
@@ -18,14 +18,25 @@ export class RegistroComponent implements OnInit {
         password: ['', [Validators.required, Validators.minLength(6)]],
         passwordConfirmacion: ['', [Validators.required]]
     }, {
-        validators: [ this.validatorService.camposIguales('password', 'passwordConfirmacion') ]
+        validators: [this.validatorService.camposIguales('password', 'passwordConfirmacion')]
     })
 
+    get emailErrorMsg(): string {
+        console.log('tick');
+        const errors = this.formulario.get('email')?.errors
+
+        if (errors?.['required']) { return 'Email obligatorio' }
+        else if (errors?.['pattern']) { return 'El formato no es válido'; }
+        else if (errors?.['emailTomado']) { return 'El email ya esta en uso'; }
+
+        return '';
+    }
+
     constructor(
-        private fb: FormBuilder, 
+        private fb: FormBuilder,
         private validatorService: ValidatorService,
         private ev: EmailValidatorService
-    ){}
+    ) { }
 
     ngOnInit(): void {
         this.formulario.reset({
@@ -33,29 +44,30 @@ export class RegistroComponent implements OnInit {
             email: 'test1@test.com',
             username: 'adrcorona13'
         })
+        this.formulario.markAllAsTouched()
     }
 
-    validarCampo(campo: string){
-        return this.formulario.controls[campo].touched 
+    validarCampo(campo: string) {
+        return this.formulario.controls[campo].touched
             && this.formulario.controls[campo].invalid;
     }
 
-    emailRequerido(){
-        return this.formulario.get('email')?.errors?.['required']
-            && this.formulario.get('email')?.touched;
-    }
+    // emailRequerido(){
+    //     return this.formulario.get('email')?.errors?.['required']
+    //         && this.formulario.get('email')?.touched;
+    // }
 
-    emailFormato(){
-        return this.formulario.get('email')?.errors?.['pattern']
-            && this.formulario.get('email')?.touched;
-    }
+    // emailFormato(){
+    //     return this.formulario.get('email')?.errors?.['pattern']
+    //         && this.formulario.get('email')?.touched;
+    // }
 
-    emailEnUso(){
-        return this.formulario.get('email')?.errors?.['emailTomado']
-            && this.formulario.get('email')?.touched;
-    }
+    // emailEnUso(){
+    //     return this.formulario.get('email')?.errors?.['emailTomado']
+    //         && this.formulario.get('email')?.touched;
+    // }
 
-    crear(){
+    crear() {
         console.log(this.formulario.value);
         this.formulario.markAllAsTouched();
     }

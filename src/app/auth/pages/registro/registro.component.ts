e3c1cea235a373836fd1c5920664a1f6c80ff131
Adrian Pineda <adrian.pc13@hotmail.com>
2023-02-23 22:21:40 -0600

    validacion del username

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-registro',
@@ -9,18 +9,30 @@ export class RegistroComponent implements OnInit {
 
     regexNombreApellido: string = '([a-zA-Z]+) ([a-zA-Z]+)';
     regexEmail: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+    noPuedeSerStrider(control: FormControl){
+        const valor = control.value?.trim().toLowerCase();
+        if (valor === 'strider') {
+            return {
+                noStrider: true
+            }
+        }
+        return null;
+    }
+
 
     formulario: FormGroup = this.fb.group({
         nombre: ['', [Validators.required, Validators.pattern(this.regexNombreApellido)]],
         email: ['', [Validators.required, Validators.pattern(this.regexEmail)]],
+        username: ['', [Validators.required, this.noPuedeSerStrider]]
     })
 
     constructor(private fb: FormBuilder){}
 
     ngOnInit(): void {
         this.formulario.reset({
-            nombre: 'Adrian',
-            email: 'adrian@gmail.com'
+            nombre: 'Adrian Pineda',
+            email: 'adrian@gmail.com',
+            username: 'adrcorona13'
         })
     }
 
